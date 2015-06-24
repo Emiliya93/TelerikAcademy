518886a5b24c68fff30e3b608f5fb302604a2365
Emiliya93 <e.georgieva93@gmail.com>
2015-06-24 16:51:04 +0300

    Refactored tasks and added task 4.

---
+++ HighQualityCode/NamingIdentifiersHomework/Task1. class_123inC/Engine.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *Refactor the following examples to produce code with well-named C# identifiers. 
+ */
+namespace PrintBoolOnConsole
+{
+    using System;
+
+    class PrintInConsole
+    {
+        const int maxCount = 6;
+
+        public static void Main()
+        {
+            Printer print = new Printer();
+            print.PrintBool(true);
+        }
+    }
+}
---
+++ HighQualityCode/NamingIdentifiersHomework/Task2.Make_ЧуекinC/Engine.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Refactor the following examples to produce code with well-named C# identifiers.
+ */
+namespace MakePerson
+{
+    public class MakePerson
+    {
+        public static void Main()
+        {
+            Person new_Чуек = new Person(5);
+        }
+    }
+}
