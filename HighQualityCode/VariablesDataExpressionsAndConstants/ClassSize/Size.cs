a7596ed4adcfb2f7799c1dd3131d7d8b82fb092e
Emiliya93 <e.georgieva93@gmail.com>
2015-07-18 10:14:02 +0300

    Added variables, data, expressions and constants homework.

---
+++ HighQualityCode/VariablesDataExpressionsAndConstants/ClassSize/Size.cs
@@ -0,0 +1,49 @@
+﻿/* Refactor the following code to use proper variable naming and simplified expressions:
+
+    public class Size
+    {
+        public double wIdTh, Viso4ina;
+        public Size(double w, double h)
+        {
+            this.wIdTh = w;
+            this.Viso4ina = h;
+        }
+
+        public static Size GetRotatedSize(
+            Size s, double angleOfTheFigureThatWillBeRotaed)
+        {
+            return new Size(
+                Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.wIdTh +
+                    Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.Viso4ina,
+                Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.wIdTh +
+                    Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.Viso4ina);
+        }
+    }
+*/
+namespace SizeCalculator
+{
+    using System;
+
+    public class Size
+    {
+        private double width, height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(Size size, double angleOfTheFigureThatWillBeRotaed)
+        {
+            double cosineOfAngle = Math.Cos(angleOfTheFigureThatWillBeRotaed),
+                   sineOfAngle = Math.Sin(angleOfTheFigureThatWillBeRotaed),
+                   absCosineOfAngle = Math.Abs(cosineOfAngle),
+                   absSineOfAngle = Math.Abs(sineOfAngle),
+                   rotatedFigureWidth = (absCosineOfAngle * size.width) + (absSineOfAngle * size.height),
+                   rotatedFigureHeight = (absSineOfAngle * size.width) + (absCosineOfAngle * size.height);
+
+            return new Size(rotatedFigureWidth, rotatedFigureHeight);
+        }
+    }
+}
---
+++ HighQualityCode/VariablesDataExpressionsAndConstants/MethodPrintStatistics/PrintStatistics.cs
@@ -0,0 +1,107 @@
+﻿/* Refactor the following code to apply variable usage and naming best practices:
+
+    public void PrintStatistics(double[] arr, int count)
+    {
+        double max, tmp;
+        for (int i = 0; i < count; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        PrintMax(max);
+        tmp = 0;
+        max = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (arr[i] < max)
+            {
+                max = arr[i];
+            }
+        }
+        PrintMin(max);
+
+        tmp = 0;
+        for (int i = 0; i < count; i++)
+        {
+            tmp += arr[i];
+        }
+        PrintAvg(tmp/count);
+    }
+*/
+
+using System;
+namespace PrintStatistics 
+{
+    public class Printer 
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max = FindMaxNumberOfArray(arr, count);
+            PrintMax(max);
+
+            double min = FindMinNumberOfArray(arr, count);
+            PrintMin(min);
+
+            double average = FindAverageOfArray(arr, count);
+            PrintAvg(average);
+        }
+
+        private static double FindAverageOfArray(double[] arr, int count)
+        {
+            double sumOfAllElements = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sumOfAllElements += arr[i];
+            }
+
+            double average = sumOfAllElements / count;
+            return average;
+        }
+
+        private static double FindMinNumberOfArray(double[] arr, int count)
+        {
+            double min = arr[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        private static double FindMaxNumberOfArray(double[] arr, int count)
+        {
+            double max = arr[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        private void PrintAvg(double average)
+        {
+            Console.WriteLine("Average sum is: {0}", average);
+        }
+
+        private void PrintMin(double min)
+        {
+            Console.WriteLine("Minimal number is: {0}", min);
+        }
+
+        private void PrintMax(double max)
+        {
+            Console.WriteLine("Maximal number is: {0}", max);
+        }
+    }
+    }
